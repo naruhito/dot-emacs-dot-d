34b8d64dacd4c0aa8b62f3f4f2fba65df10e143f
naruhito <naruhito78@gmail.com>
2015-01-20 07:02:05 +0900

    分割したウィンドウのサイズを hjkl で変更する関数を導入

--- conf/init-emacs.el
+++ conf/init-emacs.el
@@ -66,6 +66,7 @@
 (gdefkey "C-x y" 'yas/register-oneshot-snippet)
 (gdefkey "C-x C-y" 'yas/expand-oneshot-snippet)
 (gdefkey "C-c r" 'query-replace-regexp)
+(gdefkey "C-c C-r" 'window-resizer)
 (gdefkey "C-x C-c" 'server-edit)
 (gdefkey "C-x r i" 'string-insert-rectangle)
 (gdefkey "C-x C-l" 'set-buffer-file-coding-system)
@@ -240,6 +241,42 @@
 (defun describe-face-at-point ()
   (interactive)
   (message "%s" (get-text-property (point) 'face)))
+
+;; 分割したウィンドウのサイズを hjkl で変更する関数
+(defun window-resizer ()
+  "Control window size and position."
+  (interactive)
+  (let ((window-obj (selected-window))
+        (current-width (window-width))
+        (current-height (window-height))
+        (dx (if (= (nth 0 (window-edges)) 0) 1
+              -1))
+        (dy (if (= (nth 1 (window-edges)) 0) 1
+              -1))
+        action c)
+    (catch 'end-flag
+      (while t
+        (setq action
+              (read-key-sequence-vector (format "size[%dx%d]"
+                                                (window-width)
+                                                (window-height))))
+        (setq c (aref action 0))
+        (cond ((= c ?l)
+               (enlarge-window-horizontally dx))
+              ((= c ?h)
+               (shrink-window-horizontally dx))
+              ((= c ?j)
+               (enlarge-window dy))
+              ((= c ?k)
+               (shrink-window dy))
+              ;; otherwise
+              (t
+               (let ((last-command-char (aref action 0))
+                     (command (key-binding action)))
+                 (when command
+                   (call-interactively command)))
+               (message "Quit")
+               (throw 'end-flag t)))))))
 
 ;; 画面サイズ・フォント・色・その他
 (cond
