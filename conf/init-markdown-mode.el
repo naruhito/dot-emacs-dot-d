f428e0b91dcd48e011b4e38d66be8a4ef84ac087
naruhito <naruhito78@gmail.com>
2015-01-31 14:45:55 +0900

    markdown-mode の face を変更

--- conf/init-markdown-mode.el
+++ conf/init-markdown-mode.el
@@ -2,3 +2,12 @@
 (require 'markdown-mode)
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
+
+(custom-set-faces
+ '(markdown-header-face ((((class color) (min-colors 88) (background dark)) (:foreground "#DB3D36" :weight bold))))
+ '(markdown-header-rule-face ((t (:inherit markdown-header-face))))
+ '(markdown-header-delimiter-face ((t (:inherit markdown-header-face))))
+ '(markdown-link-face ((t (:inherit font-lock-builtin-face :foreground "#D68E31"))))
+ '(markdown-pre-face ((t (:inherit font-lock-constant-face :foreground "#3BAF75"))))
+ '(markdown-list-face ((t (:inherit font-lock-builtin-face :foreground "#FBD01D" :weight bold))))
+ )
