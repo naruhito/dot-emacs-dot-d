114d5f61114c56610607d62251765380654b9ba0
naruhito <naruhito78@gmail.com>
2016-01-29 02:05:41 +0900

    sbt-mode は v23 には非対応

--- conf/init-sbt-mode.el
+++ conf/init-sbt-mode.el
@@ -1,4 +1,6 @@
-;;; scala-mode.el --- Functions for discovering the current sbt project
-(require 'utilities "~/.emacs.d/utilities")
-(add-to-load-path "~/.emacs.d/elisp/sbt-mode")
-(require 'sbt-mode)
+;;; sbt-mode.el --- Functions for discovering the current sbt project
+(when (<= 24 emacs-major-version) ;23以前には未対応
+  (require 'utilities "~/.emacs.d/utilities")
+  (add-to-load-path "~/.emacs.d/elisp/sbt-mode")
+  (require 'sbt-mode)
+  )
