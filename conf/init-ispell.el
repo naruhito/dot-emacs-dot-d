854dc8ee68af8aa28a90ea51bf6a5df25d4604ca
naruhito <naruhito78@gmail.com>
2016-06-12 01:20:59 +0900

    aspell (ispell) 設定サンプルを追加

--- conf/init-ispell.el
+++ conf/init-ispell.el
@@ -1,6 +1,6 @@
 ;;; ispell.el --- interface to International Ispell Versions 3.1 and 3.2
 ;; ` ': Accept word this time. (SPC)
 ;; `i': Accept word and insert into private dictionary.
-(require 'ispell)                       ; 動作要件: aspell
+(require 'ispell)                       ; 動作要件: aspell を brew や yum でインストール
 (setq ispell-program-name "aspell")
 (setq ispell-personal-dictionary "~/.emacs.d/var/aspell-pdict")
