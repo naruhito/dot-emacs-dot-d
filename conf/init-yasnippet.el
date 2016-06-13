b757c15a55f7e26fce8d7cd3989fa03eec42a865
naruhito <naruhito78@gmail.com>
2016-06-13 22:11:36 +0900

    yasnippet の設定を修正

--- conf/init-yasnippet.el
+++ conf/init-yasnippet.el
@@ -2,9 +2,11 @@
 (if (> 24 emacs-major-version)
     (require 'cl-lib))
 (require 'yasnippet)
-(yas-global-mode 1)
 
 ;; スニペットの保存場所
 (setq yas-snippet-dirs
       '("~/.emacs.d/var/snippets"       ; 一時的な用途のスニペット
         "~/.emacs.d/snippets"))
+
+;; 全バッファで有効化
+(yas-global-mode 1)
