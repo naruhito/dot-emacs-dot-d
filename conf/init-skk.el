be5b8c44ee38da84c67a7fb6510f9b4bf1575533
naruhito <naruhito78@gmail.com>
2016-07-18 01:22:28 +0900

    Eshell 起動直後に SSK を有効化する

--- conf/init-skk.el
+++ conf/init-skk.el
@@ -54,6 +54,7 @@
 (add-hook 'find-file-hooks 'skk-mode-on-with-latin)
 (add-hook 'after-revert-hook 'skk-mode-on-with-latin)
 (add-hook 'markdown-mode-hook 'skk-mode-on-with-latin)
+(add-hook 'eshell-mode-hook 'skk-mode-on-with-latin)
 (defun skk-mode-on-with-latin ()
   (interactive)
   (skk-mode)
