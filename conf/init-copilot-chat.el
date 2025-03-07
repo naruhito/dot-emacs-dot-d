0cf95f97c96f8053e7eb8b9ef6945cc26cb094eb
Naruhito <naruhito78@gmail.com>
2025-03-08 03:26:10 +0900

    install copilot-chat

--- conf/init-copilot-chat.el
+++ conf/init-copilot-chat.el
@@ -1,6 +1,5 @@
 ;;; copilot-chat.el --- Copilot chat interface -*- indent-tabs-mode: nil; lisp-indent-offset: 2; lexical-binding: t -*-
 (use-package copilot-chat)
-(setopt copilot-chat-frontend 'markdown)
 (with-eval-after-load 'copilot-chat-common
   (setq my/copilot-chat-prompt-original copilot-chat-prompt)
   (setopt copilot-chat-prompt (concat my/copilot-chat-prompt-original "\n出力には日本語を用います")))
